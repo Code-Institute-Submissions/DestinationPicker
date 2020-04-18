cbfbf894309dfd174af3b666c961eb4da4dbce15
FrancesVugts <francesvugts@hotmail.com>
2020-04-18 09:49:58 +0000

    Change the JQuery function to link from question 1 to question 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
+    // Got the window.location.href from the reactions on the following link: https://stackoverflow.com/questions/16959476/how-to-go-to-a-url-using-jquery
   $(".btn.q1").click(function() {
-      $(this).css("color", "red");
+      window.location.href = "question-two.html";
   });  
 
 });
