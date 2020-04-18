adaa5d09741c838fe1baeb18dd51b5f613785267
FrancesVugts <francesvugts@hotmail.com>
2020-04-18 13:50:49 +0000

    Change from 1 HTML page per question, to all questions in 1 HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
-    // Got the window.location.href from the reactions on the following link: https://stackoverflow.com/questions/16959476/how-to-go-to-a-url-using-jquery
   $(".btn.q1").click(function() {
-      window.location.href = "question-two.html";
-  });
-  
-  $(".btn.q2").click(function() {
-      window.location.href = "question-three.html";
+      $(".question1").css("display", "none");
+      $(".question2").css("display", "block");
   });
 
 });
