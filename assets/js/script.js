3f55295ce13237dbc480ed5680930825cbaaed1f
FrancesVugts <francesvugts@hotmail.com>
2020-04-18 09:57:48 +0000

    Add HTML page for question three

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     // Got the window.location.href from the reactions on the following link: https://stackoverflow.com/questions/16959476/how-to-go-to-a-url-using-jquery
   $(".btn.q1").click(function() {
       window.location.href = "question-two.html";
-  });  
+  });
+  
+  $(".btn.q2").click(function() {
+      window.location.href = "question-three.html";
+  });
 
 });
