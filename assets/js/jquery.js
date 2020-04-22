858645bdeb23356b7ef2a183eccf26c1fa8dfe13
FrancesVugts <francesvugts@hotmail.com>
2020-04-22 16:38:58 +0000

    Get rid of

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -5,32 +5,32 @@ $(document).ready(function() {
         window.location.href = "questions.html";
     });
 
-    $(".btnsQ1>.btn").click(function() {
+    $(".question1").find(".btn").click(function() {
         $(".question1").css("display", "none");
         $(".question2").css("display", "block");
     });
 
-    $(".btnsQ2>.btn").click(function() {
+    $(".question2").find(".btn").click(function() {
         $(".question2").css("display", "none");
         $(".question3").css("display", "block");
     });
 
-    $(".btnsQ3>.btn").click(function() {
+    $(".question3").find(".btn").click(function() {
         $(".question3").css("display", "none");
         $(".question4").css("display", "block");
     });
 
-    $(".btnsQ4>.btn").click(function() {
+    $(".question4").find(".btn").click(function() {
         $(".question4").css("display", "none");
         $(".question5").css("display", "block");
     });
 
-    $(".btnsQ5>.btn").click(function() {
+    $(".question5").find(".btn").click(function() {
         $(".question5").css("display", "none");
         $(".question6").css("display", "block");
     });
 
-    $(".btnsQ6>.btn").click(function() {
+    $(".question6").find(".btn").click(function() {
         $(".question6").css("display", "none");
         $(".question7").css("display", "block");
     });
