9d078ab6fbf0aa405008551387fb1784d6f8a0db
FrancesVugts <francesvugts@hotmail.com>
2020-04-25 20:32:50 +0000

    Add in the other questions again

--- assets/js/count.js
+++ assets/js/count.js
@@ -7,22 +7,22 @@ var namibia = {score: 0, costs: "€2500-3500", surrounding: "Nature", season: "
 
 // Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
 $(".btn").click(function() {
-    if($(this).text() == belgium.costs || $(this).text() == belgium.surrounding) {
+    if($(this).text() == belgium.costs || $(this).text() == belgium.surrounding || $(this).text() == belgium.season || $(this).text() == belgium.traveltime || $(this).text() == belgium.weather || $(this).text() == belgium.transport || $(this).text() == belgium.culture) {
         belgium.score ++;
     }
-    if($(this).text() == iceland.costs || $(this).text() == iceland.surrounding) {
+    if($(this).text() == iceland.costs || $(this).text() == iceland.surrounding || $(this).text() == iceland.season || $(this).text() == iceland.traveltime || $(this).text() == iceland.weather || $(this).text() == iceland.transport || $(this).text() == iceland.culture) {
         iceland.score ++;
     }
-    if($(this).text() == italy.costs || $(this).text() == italy.surrounding) {
+    if($(this).text() == italy.costs || $(this).text() == italy.surrounding || $(this).text() == italy.season || $(this).text() == italy.traveltime || $(this).text() == italy.weather || $(this).text() == italy.transport || $(this).text() == italy.culture) {
         italy.score ++;
     }
-    if($(this).text() == finland.costs || $(this).text() == finland.surrounding) {
+    if($(this).text() == finland.costs || $(this).text() == finland.surrounding || $(this).text() == finland.season || $(this).text() == finland.traveltime || $(this).text() == finland.weather || $(this).text() == finland.transport || $(this).text() == finland.culture) {
         finland.score ++;
     }
-    if($(this).text() == madagascar.costs || $(this).text() == madagascar.surrounding) {
+    if($(this).text() == madagascar.costs || $(this).text() == madagascar.surrounding || $(this).text() == madagascar.season || $(this).text() == madagascar.traveltime || $(this).text() == madagascar.weather || $(this).text() == madagascar.transport || $(this).text() == madagascar.culture) {
         madagascar.score ++;
     }
-    if($(this).text() == namibia.costs || $(this).text() == namibia.surrounding) {
+    if($(this).text() == namibia.costs || $(this).text() == namibia.surrounding || $(this).text() == namibia.season || $(this).text() == namibia.traveltime || $(this).text() == namibia.weather || $(this).text() == namibia.transport || $(this).text() == namibia.culture) {
         namibia.score ++;
     }
 });
