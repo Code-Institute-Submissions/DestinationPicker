60b0fae9b5f3716cc4d38ec40e1c6f5549c95d91
FrancesVugts <francesvugts@hotmail.com>
2020-04-25 16:54:07 +0000

    Make function beter by using the actual buttontext

--- assets/js/count.js
+++ assets/js/count.js
@@ -5,9 +5,25 @@ var finland = {score: 0, costs: "€750-1500", surrounding: "Nature", season: "W
 var madagascar = {score: 0, costs: "€1500-2500", surrounding: "Nature", season: "Spring", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
 var namibia = {score: 0, costs: "€2500-3500", surrounding: "Nature", season: "Autumn", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
 
+// Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
 $(".question1").find(".btn").click(function() {
-        if("€0-750" == belgium.costs) {
+        if($(this).text() == belgium.costs) {
             belgium.score ++;
+        }
+        if($(this).text() == iceland.costs) {
+            iceland.score ++;
+        }
+        if($(this).text() == italy.costs) {
+            italy.score ++;
+        }
+        if($(this).text() == finland.costs) {
+            finland.score ++;
+        }
+        if($(this).text() == madagascar.costs) {
+            madagascar.score ++;
+        }
+        if($(this).text() == namibia.costs) {
+            namibia.score ++;
         }
     });
 
