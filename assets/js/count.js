70586c3a7d6263c4f7a272fbc330a30df2c95bc2
FrancesVugts <francesvugts@hotmail.com>
2020-04-25 15:49:16 +0000

    Add console.log to count.js to check what my changes will do to the scores

--- assets/js/count.js
+++ assets/js/count.js
@@ -4,3 +4,5 @@ var italy = {score: 0, costs: "€750-1500", surrounding: "Combination", season:
 var finland = {score: 0, costs: "€750-1500", surrounding: "Nature", season: "Winter", traveltime: "Medium", weather: "Cold", transport: "Plane", culture: "A little"};
 var madagascar = {score: 0, costs: "€1500-2500", surrounding: "Nature", season: "Spring", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
 var namibia = {score: 0, costs: "€2500-3500", surrounding: "Nature", season: "Autumn", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
+
+console.log(belgium.score, iceland.score, italy.score, finland.score, madagascar.score, namibia.score)
