d43b5e7480ac4a4c9b0394aa4ff6503673dff1b5
FrancesVugts <francesvugts@hotmail.com>
2020-04-27 17:31:26 +0000

    Change the determination for the first choice destenation

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,9 +1,10 @@
-var belgium = {score: 0, attributes: ["€0-750", "Combination", "Summer", "Short", "Normal", "Car", "No"]};
-var iceland = {score: 0, attributes: ["€750-1500", "Nature", "Summer", "Short", "Cold", "Plane", "A little"]};
-var italy = {score: 0, attributes: ["€750-1500", "Combination", "Summer", "Long", "Hot", "Car", "A little"]};
-var finland = {score: 0, attributes: ["€750-1500", "Nature", "Winter", "Medium", "Cold", "Plane", "A little"]};
-var madagascar = {score: 0, attributes: ["€1500-2500", "Nature", "Spring", "Long", "Hot", "Plane", "A lot"]};
-var namibia = {score: 0, attributes: ["€2500-3500", "Nature", "Autumn", "Long", "Hot", "Plane", "A lot"]};
+var belgium = {name: "belgium", score: 0, attributes: ["€0-750", "Combination", "Summer", "Short", "Normal", "Car", "No"]};
+var iceland = {name: "iceland", score: 0, attributes: ["€750-1500", "Nature", "Summer", "Short", "Cold", "Plane", "A little"]};
+var italy = {name: "italy", score: 0, attributes: ["€750-1500", "Combination", "Summer", "Long", "Hot", "Car", "A little"]};
+var finland = {name: "finland", score: 0, attributes: ["€750-1500", "Nature", "Winter", "Medium", "Cold", "Plane", "A little"]};
+var madagascar = {name: "madagascar", score: 0, attributes: ["€1500-2500", "Nature", "Spring", "Long", "Hot", "Plane", "A lot"]};
+var namibia = {name: "namibia", score: 0, attributes: ["€2500-3500", "Nature", "Autumn", "Long", "Hot", "Plane", "A lot"]};
+var destinations = [belgium, iceland, italy, finland, madagascar, namibia];
 
 // Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
 $(".btn").click(function() {
@@ -40,29 +41,41 @@ $(".btn").click(function() {
 });
 
 $(".question7").click(function() {
-    if(belgium.score>iceland.score && belgium.score>italy.score && belgium.score>finland.score && belgium.score>madagascar.score && belgium.score>namibia.score) {
-        window.location.href = "results/belgium.html";
-    } else {
-        if(iceland.score>italy.score && iceland.score>finland.score && iceland.score>madagascar.score && iceland.score>namibia.score) {
-            window.location.href = "results/iceland.html";
-        } else {
-            if(italy.score>finland.score && italy.score>madagascar.score && italy.score>namibia.score) {
-                window.location.href = "results/italy.html";
-            } else {
-                if(finland.score>madagascar.score && finland.score>namibia.score) {
-                    window.location.href = "results/finland.html";
-                } else {
-                    if(madagascar.score>namibia.score) {
-                        window.location.href = "results/madagascar.html";
-                    } else {
-                        window.location.href = "results/namibia.html";
-                    }
-                }
-            }
+    var highestScore = 0;
+    var firstChoice = null;
+
+    for (var i in destinations) {
+        if (destinations[i].score > highestScore) {
+            highestScore = destinations[i].score;
+            firstChoice = destinations[i].name;
         }
     }
+
+    window.location.href = "results/" + firstChoice + ".html";
+
+    // if(belgium.score>iceland.score && belgium.score>italy.score && belgium.score>finland.score && belgium.score>madagascar.score && belgium.score>namibia.score) {
+    //     window.location.href = "results/belgium.html";
+    // } else {
+    //     if(iceland.score>italy.score && iceland.score>finland.score && iceland.score>madagascar.score && iceland.score>namibia.score) {
+    //         window.location.href = "results/iceland.html";
+    //     } else {
+    //         if(italy.score>finland.score && italy.score>madagascar.score && italy.score>namibia.score) {
+    //             window.location.href = "results/italy.html";
+    //         } else {
+    //             if(finland.score>madagascar.score && finland.score>namibia.score) {
+    //                 window.location.href = "results/finland.html";
+    //             } else {
+    //                 if(madagascar.score>namibia.score) {
+    //                     window.location.href = "results/madagascar.html";
+    //                 } else {
+    //                     window.location.href = "results/namibia.html";
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 });
 
 $(this).click(function() {
-    console.log(belgium.score, iceland.score, italy.score, finland.score, madagascar.score, namibia.score)
+    console.log(belgium.score, iceland.score, italy.score, finland.score, madagascar.score, namibia.score);
 });
