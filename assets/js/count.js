86f69278404093ea991e28385b30abc2e559d228
FrancesVugts <francesvugts@hotmail.com>
2020-04-27 14:04:19 +0000

    Cleanup for the count of the other countries

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,9 +1,9 @@
 var belgium = {score: 0, attributes: ["€0-750", "Combination", "Summer", "Short", "Normal", "Car", "No"]};
 var iceland = {score: 0, attributes: ["€750-1500", "Nature", "Summer", "Short", "Cold", "Plane", "A little"]};
-var italy = {score: 0, costs: "€750-1500", surrounding: "Combination", season: "Summer", traveltime: "Long", weather: "Hot", transport: "Car", culture: "A little"};
-var finland = {score: 0, costs: "€750-1500", surrounding: "Nature", season: "Winter", traveltime: "Medium", weather: "Cold", transport: "Plane", culture: "A little"};
-var madagascar = {score: 0, costs: "€1500-2500", surrounding: "Nature", season: "Spring", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
-var namibia = {score: 0, costs: "€2500-3500", surrounding: "Nature", season: "Autumn", traveltime: "Long", weather: "Hot", transport: "Plane", culture: "A lot"};
+var italy = {score: 0, attributes: ["€750-1500", "Combination", "Summer", "Long", "Hot", "Car", "A little"]};
+var finland = {score: 0, attributes: ["€750-1500", "Nature", "Winter", "Medium", "Cold", "Plane", "A little"]};
+var madagascar = {score: 0, attributes: ["€1500-2500", "Nature", "Spring", "Long", "Hot", "Plane", "A lot"]};
+var namibia = {score: 0, attributes: ["€2500-3500", "Nature", "Autumn", "Long", "Hot", "Plane", "A lot"]};
 
 // Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
 $(".btn").click(function() {
@@ -17,17 +17,25 @@ $(".btn").click(function() {
             iceland.score ++;
         }
     }
-    if($(this).text() == italy.costs || $(this).text() == italy.surrounding || $(this).text() == italy.season || $(this).text() == italy.traveltime || $(this).text() == italy.weather || $(this).text() == italy.transport || $(this).text() == italy.culture) {
-        italy.score ++;
+    for (var i in italy.attributes) {
+        if ($(this).text() == italy.attributes[i]) {
+            italy.score ++;
+        }
     }
-    if($(this).text() == finland.costs || $(this).text() == finland.surrounding || $(this).text() == finland.season || $(this).text() == finland.traveltime || $(this).text() == finland.weather || $(this).text() == finland.transport || $(this).text() == finland.culture) {
-        finland.score ++;
+    for (var i in finland.attributes) {
+        if ($(this).text() == finland.attributes[i]) {
+            finland.score ++;
+        }
     }
-    if($(this).text() == madagascar.costs || $(this).text() == madagascar.surrounding || $(this).text() == madagascar.season || $(this).text() == madagascar.traveltime || $(this).text() == madagascar.weather || $(this).text() == madagascar.transport || $(this).text() == madagascar.culture) {
-        madagascar.score ++;
+    for (var i in madagascar.attributes) {
+        if ($(this).text() == madagascar.attributes[i]) {
+            madagascar.score ++;
+        }
     }
-    if($(this).text() == namibia.costs || $(this).text() == namibia.surrounding || $(this).text() == namibia.season || $(this).text() == namibia.traveltime || $(this).text() == namibia.weather || $(this).text() == namibia.transport || $(this).text() == namibia.culture) {
-        namibia.score ++;
+    for (var i in namibia.attributes) {
+        if ($(this).text() == namibia.attributes[i]) {
+            namibia.score ++;
+        }
     }
 });
 
