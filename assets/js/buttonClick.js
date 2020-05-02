c021a45cfd4f51e5abbe861c578edb48e19e921b
FrancesVugts <francesvugts@hotmail.com>
2020-05-02 09:18:34 +0000

    Add comment for function in buttonClick.js

--- assets/js/buttonClick.js
+++ assets/js/buttonClick.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         window.location.href = "questions.html";
     });
 
+    // With this function I will hide the answered question and show the following question
     $(".questions").find(".btn").click(function() {
         $(this).parent().parent().css("display", "none");
         $(this).parent().parent().next().css("display", "block");
