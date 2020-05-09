3bef57693f0b444e5626941ae46f1d4737e0b7b0
FrancesVugts <francesvugts@hotmail.com>
2020-05-09 07:55:51 +0000

    Add document.ready functions to my javascript

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,34 +1,38 @@
-var belgium = {name: "belgium", score: 0, attributes: ["€0-750", "Combination", "Summer", "Short", "Normal", "Car", "No"]};
-var iceland = {name: "iceland", score: 0, attributes: ["€750-1500", "Nature", "Summer", "Short", "Cold", "Plane", "A little"]};
-var italy = {name: "italy", score: 0, attributes: ["€750-1500", "Combination", "Summer", "Long", "Hot", "Car", "A little"]};
-var finland = {name: "finland", score: 0, attributes: ["€750-1500", "Nature", "Winter", "Medium", "Cold", "Plane", "A little"]};
-var madagascar = {name: "madagascar", score: 0, attributes: ["€1500-2500", "Nature", "Spring", "Long", "Hot", "Plane", "A lot"]};
-var namibia = {name: "namibia", score: 0, attributes: ["€2500-3500", "Nature", "Autumn", "Long", "Hot", "Plane", "A lot"]};
-var destinations = [belgium, iceland, italy, finland, madagascar, namibia];
+$(document).ready(function() {
 
-// With this function I compare the answer on the clicked button with the attributes of each country
-// When there is a match, that country will get a point
-// Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
-$(".btn").click(function() {
-    for (var i in destinations) {
-        for (var j in destinations[i].attributes) {
-            if ($(this).text() == destinations[i].attributes[j]) {
-                destinations[i].score ++;
+    var belgium = {name: "belgium", score: 0, attributes: ["€0-750", "Combination", "Summer", "Short", "Normal", "Car", "No"]};
+    var iceland = {name: "iceland", score: 0, attributes: ["€750-1500", "Nature", "Summer", "Short", "Cold", "Plane", "A little"]};
+    var italy = {name: "italy", score: 0, attributes: ["€750-1500", "Combination", "Summer", "Long", "Hot", "Car", "A little"]};
+    var finland = {name: "finland", score: 0, attributes: ["€750-1500", "Nature", "Winter", "Medium", "Cold", "Plane", "A little"]};
+    var madagascar = {name: "madagascar", score: 0, attributes: ["€1500-2500", "Nature", "Spring", "Long", "Hot", "Plane", "A lot"]};
+    var namibia = {name: "namibia", score: 0, attributes: ["€2500-3500", "Nature", "Autumn", "Long", "Hot", "Plane", "A lot"]};
+    var destinations = [belgium, iceland, italy, finland, madagascar, namibia];
+
+    // With this function I compare the answer on the clicked button with the attributes of each country
+    // When there is a match, that country will get a point
+    // Used .text() to get the text from the clicked button as suggested by ThiefMaster on the following page: https://stackoverflow.com/questions/10351658/javascript-get-custom-buttons-text-value 
+    $(".btn").click(function() {
+        for (var i in destinations) {
+            for (var j in destinations[i].attributes) {
+                if ($(this).text() == destinations[i].attributes[j]) {
+                    destinations[i].score ++;
+                }
             }
         }
-    }
-});
+    });
 
-// With this function I compare the scores and link to the page of the winning country
-$(".question7").find(".btn").click(function() {
-    // Got some help from my mentor to get the code to determine one of the first choice destinations when there is a draw
-    var highestScore = 0;
-    var firstChoice = null;
-    for (var i in destinations) {
-        if (destinations[i].score > highestScore) {
-            highestScore = destinations[i].score;
-            firstChoice = destinations[i].name;
+    // With this function I compare the scores and link to the page of the winning country
+    $(".question7").find(".btn").click(function() {
+        // Got some help from my mentor to get the code to determine one of the first choice destinations when there is a draw
+        var highestScore = 0;
+        var firstChoice = null;
+        for (var i in destinations) {
+            if (destinations[i].score > highestScore) {
+                highestScore = destinations[i].score;
+                firstChoice = destinations[i].name;
+            }
         }
-    }
-    window.location.href = `results/${firstChoice}.html`;
+        window.location.href = `results/${firstChoice}.html`;
+    });
+
 });
--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,12 +1,16 @@
-// Used the code institute lessons to create this function for mailing me
-function sendMail(contactForm) {
-    emailjs.send("gmail", "destination_picker", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
-    });
-    $("#myModal").modal("hide");
-    $("#name, #email, #message").val("");
-    return alert('Thank you for your message!')
-    return false;
-}
+$(document).ready(function() {
+
+    // Used the code institute lessons to create this function for mailing me
+    function sendMail(contactForm) {
+        emailjs.send("gmail", "destination_picker", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "message": contactForm.message.value
+        });
+        $("#myModal").modal("hide");
+        $("#name, #email, #message").val("");
+        return alert('Thank you for your message!')
+        return false;
+    }
+
+});
