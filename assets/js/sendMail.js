a0fc66c4e9ab9ab7076d1d43057c560c621b5926
FrancesVugts <francesvugts@hotmail.com>
2020-05-09 21:16:24 +0000

    Fix the sendMail function

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,16 +1,12 @@
-$(document).ready(function() {
-
-    // Used the code institute lessons to create this function for mailing me
-    function sendMail(contactForm) {
-        emailjs.send("gmail", "destination_picker", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.email.value,
-            "message": contactForm.message.value
-        });
-        $("#myModal").modal("hide");
-        $("#name, #email, #message").val("");
-        return alert('Thank you for your message!')
-        return false;
-    }
-
-});
+// Used the code institute lessons to create this function for mailing me
+function sendMail(contactForm) {
+    emailjs.send("gmail", "destination_picker", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    });
+    $("#myModal").modal("hide");
+    $("#name, #email, #message").val("");
+    return alert('Thank you for your message!');
+    return false;
+}
