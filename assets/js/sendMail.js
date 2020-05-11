19318063d1d32914c1e5a4596ee83395fd1eac36
FrancesVugts <francesvugts@hotmail.com>
2020-05-11 17:11:53 +0000

    Change the senMail function

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,12 +1,20 @@
-// Used the code institute lessons to create this function for mailing me
+// Function for sending a mail to the owner of the website, hiding the modal and cleering the form.
+// My mentor helped me with the try and catch.
 function sendMail(contactForm) {
-    emailjs.send("gmail", "destination_picker", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
-    });
-    $("#myModal").modal("hide");
-    $("#name, #email, #message").val("");
-    return alert('Thank you for your message!');
-    return false;
+    try {
+        emailjs.send("gmail", "destination_picker", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "message": contactForm.message.value
+        });
+        $("#myModal").modal("hide");
+        $("#name, #email, #message").val("");
+        alert("Thank you for your message!");
+        return true;
+    } catch (error) {
+        $("#myModal").modal("hide");
+        $("#name, #email, #message").val("");
+        alert("Something went wrong. Please try again later.");
+        return false;
+    }
 }
