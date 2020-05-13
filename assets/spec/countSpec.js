73df84a7f47f0efd8a479ac023fe657ec084c9a1
FrancesVugts <francesvugts@hotmail.com>
2020-05-13 16:21:15 +0000

    Add a second test with different answers

--- assets/spec/countSpec.js
+++ assets/spec/countSpec.js
@@ -77,4 +77,74 @@ describe("count function", function() {
             expect(namibia.score).toBe(1);
         });
     });
+
+    describe("answers €750-1500, Combination, Spring, Long, Hot, Plane and A lot", function() {
+        
+        it("Belgiums score should be 1", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(belgium.score).toBe(1);
+        });
+
+        it("Iceland score should be 2", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(iceland.score).toBe(2);
+        });
+
+        it("Italy score should be 4", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(italy.score).toBe(4);
+        });
+
+        it("Finland score should be 2", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(finland.score).toBe(2);
+        });
+
+        it("Madagascar score should be 5", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(madagascar.score).toBe(5);
+        });
+
+        it("Namibia score should be 4", function() {
+            count("€750-1500");
+            count("Combination");
+            count("Spring");
+            count("Long");
+            count("Hot");
+            count("Plane");
+            count("A lot");
+            expect(namibia.score).toBe(4);
+        });
+    });
+
 });
